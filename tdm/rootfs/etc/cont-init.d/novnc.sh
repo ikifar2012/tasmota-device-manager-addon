ad9984e1f77810c983af179a1bc54d6c95027d5b
Matheson Steplock <ikifar2012@users.noreply.github.com>
2021-01-09 00:28:31 -0500

    1.0.5

--- tdm/rootfs/etc/cont-init.d/novnc.sh
+++ tdm/rootfs/etc/cont-init.d/novnc.sh
@@ -4,4 +4,6 @@
 # ==============================================================================
 declare ingress_entry
 ingress_entry=$(bashio::addon.ingress_entry)
+mkdir -p /usr/share/novnc/
+touch /usr/share/novnc/vnc_lite.html
 sed -i "s#websockify#${ingress_entry#?}/novnc/websockify#g" /usr/share/novnc/vnc_lite.html
