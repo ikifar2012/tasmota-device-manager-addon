2385449332d99fa4435a23ee8d7d5c157ecde9a6
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-08-30 00:00:48 -0400

    testing

--- tdm/rootfs/etc/cont-init.d/nginx.sh
+++ tdm/rootfs/etc/cont-init.d/nginx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 # ==============================================================================
-# Configure NGINX for use with deCONZ
+# Configure NGINX for use with TDMGR
 # ==============================================================================
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
