d5eec24e93496f2358e7a94bf47ac1d702c98c0c
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-09-04 23:38:57 -0400

    move to loop

--- tdm/rootfs/custom/idle-timeout.sh
+++ tdm/rootfs/custom/idle-timeout.sh
@@ -4,18 +4,6 @@
 IDLE_THRESHOLD=$(bashio::config "timeout")
 
 while true; do
-    IDLE_TIME=$(xprintidle /1000) # Get system idle time in seconds
-
-    # Check if the system has been idle for more than the threshold
-    # and if the threshold is greater than 0 if not it will be ignored
-    if [ "$IDLE_THRESHOLD" -gt 0 ] && [ "$IDLE_TIME" -ge "$IDLE_THRESHOLD" ]; then
-        # Execute the s6 shutdown command or any other desired action
-        /run/s6/basedir/bin/halt
-    fi
-    sleep 60 # Check idle time every 60 seconds
-done
-
-while true; do
     # Get the current date and time
     current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -25,6 +13,14 @@ while true; do
     # Get the available RAM in megabytes
     available_ram=$(free -m | awk '/Mem:/ {print $NF}')
 
+    IDLE_TIME=$(xprintidle /1000) # Get system idle time in seconds
+
+    # Check if the system has been idle for more than the threshold
+    # and if the threshold is greater than 0 if not it will be ignored
+    if [ "$IDLE_THRESHOLD" -gt 0 ] && [ "$IDLE_TIME" -ge "$IDLE_THRESHOLD" ]; then
+        # Execute the s6 shutdown command or any other desired action
+        /run/s6/basedir/bin/halt
+    fi
     # Create the status message
     status_message=" $current_datetime | CPU: $cpu_usage% | RAM: $available_ram MB | IDLE TIMEOUT: $IDLE_TIME MIN"
 
