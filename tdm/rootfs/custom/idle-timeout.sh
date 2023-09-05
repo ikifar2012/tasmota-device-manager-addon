edd6b9a87cadf01aa6b4bb6e47b80e0f333bc0f1
Matheson Steplock <ikifar2012@users.noreply.github.com>
2023-09-05 01:30:42 -0400

    tweak idle alert

--- tdm/rootfs/custom/idle-timeout.sh
+++ tdm/rootfs/custom/idle-timeout.sh
@@ -13,16 +13,25 @@ while true; do
     # Get the available RAM in megabytes
     available_ram=$(free -m | awk '/Mem:/ {print $NF}')
 
-    IDLE_TIME=$(xprintidle /1000) # Get system idle time in seconds
+    IDLE_TIME=$(xprintidle) 
+    # Get system idle time in minutes
+    IDLE_TIME_MIN=$((IDLE_TIME/1000/60))
+
 
     # Check if the system has been idle for more than the threshold
     # and if the threshold is greater than 0 if not it will be ignored
     if [ "$IDLE_THRESHOLD" -gt 0 ] && [ "$IDLE_TIME" -ge "$IDLE_THRESHOLD" ]; then
         # Execute the s6 shutdown command or any other desired action
+        # send a notification to home assistant
+        bashio::log.info "System has been idle for $IDLE_TIME_MIN minutes, shutting down..."
+        message="System has been idle for $IDLE_TIME_MIN minutes, shutting down..."
+        title="Tasmota Device Manager - Idle Timeout"
+        ret=ret=$(bashio::api.supervisor POST /core/api/services/persistent_notification/create \
+        "{\"message\":\"${message}\", \"title\":\"${title}\", \"notification_id\":\"tdm\"}")
         /run/s6/basedir/bin/halt
     fi
     # Create the status message
-    status_message=" $current_datetime | CPU: $cpu_usage% | RAM: $available_ram MB | IDLE TIMEOUT: $IDLE_TIME MIN"
+    status_message=" $current_datetime | CPU: $cpu_usage% | RAM: $available_ram MB | IDLE TIMEOUT: $IDLE_TIME_MIN MIN"
 
     # Send the status message to the i3 status bar
     echo "$status_message"
